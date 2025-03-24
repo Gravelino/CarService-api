85adf5300a7f6d3a38e1d9fb8ac97e82fc53236d
Gravelino <148808322+Gravelino@users.noreply.github.com>
2025-03-24 16:03:04 +0200

    Feedback and Payment Repos fix

--- Application/Interfaces/IFeedbackRepository.cs
+++ Application/Interfaces/IFeedbackRepository.cs
@@ -2,7 +2,7 @@ using Application.Models;
 
 namespace Persistence.Repositories.Interfaces;
 
-public interface IFeedbackRepository :IRepository<Feedback>
+public interface IFeedbackRepository :ISoftDeletableRepository<Feedback>
 {
     Task<IEnumerable<Feedback>> GetFeedbackForVisitAsync(int visitId);
     Task<double> GetAverageRatingForServiceAsync(int serviceId);
--- Application/Interfaces/IPaymentRepository.cs
+++ Application/Interfaces/IPaymentRepository.cs
@@ -2,7 +2,7 @@ using Application.Models;
 
 namespace Persistence.Repositories.Interfaces;
 
-public interface IPaymentRepository : IRepository<Payment>
+public interface IPaymentRepository : ISoftDeletableRepository<Payment>
 {
     Task<IEnumerable<Payment>> GetPaymentsByVisitIdAsync(int visitId);
     Task<decimal> GetTotalPaymentsForVisitAsync(int visitId);
